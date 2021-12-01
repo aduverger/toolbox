bae00c654edd7c0a2fcc614b9d8c2bab3c7df5c1
Alexandre Duverger <alexandre.duverger@protonmail.com>
2021-12-01 11:30:50 -0700

    implemented plot_learning_curves function in ml.py

--- toolbox/ml.py
+++ toolbox/ml.py
@@ -1,9 +1,25 @@
 import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
 import numpy as np
 import pandas as pd
 
 """Tools for general machine learning projects.
 """
+
+
+def plot_learning_curves(model, X, y, figsize=(9, 4), scoring=None):
+    train_sizes, train_scores, test_scores = learning_curve(
+        model, X, y, scoring=scoring
+    )
+    train_scores_mean = np.mean(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    plt.figure(figsize=figsize)
+    plt.plot(train_sizes, train_scores_mean, label="Training Score", color="black")
+    plt.plot(train_sizes, test_scores_mean, label="Test Score", color="blue")
+    plt.ylabel("Score", fontsize=14)
+    plt.xlabel("Training set size", fontsize=14)
+    plt.title("Learning curves", fontsize=18, y=1.03)
+    plt.legend()
 
 
 def reduce_memory_usage(df):
